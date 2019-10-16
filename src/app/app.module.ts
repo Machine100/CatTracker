499a358325d70477a6faf9e71b4c489dcb83c86e
Machine100 <joesmileyyy@yahoo.com>
2019-10-16 12:51:42 -0400

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { MatSliderModule, MatToolbarModule, MatButtonModule, MatIconModule} from '@angular/material'
+import {  } from '@angular/material'
+
 import { AppComponent } from './app.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NoopAnimationsModule,
+    MatSliderModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
